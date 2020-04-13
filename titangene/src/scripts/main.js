87225d252532d66147cf11a6125703822d06ade8
titangene <titangene.tw@gmail.com>
2020-04-13 13:24:28 +0800

    feat: feature carousel RWD

--- titangene/src/scripts/main.js
+++ titangene/src/scripts/main.js
@@ -11,10 +11,27 @@ const bannerSwiper = new Swiper('.banner .swiper-container', {
 
 const featureSwiper = new Swiper('.feature .swiper-container', {
   loop: true,
-  slidesPerView: 4,
-  spaceBetween: 30,
+  slidesPerView: 1,
   navigation: {
     nextEl: '.feature .swiper-button-next',
     prevEl: '.feature .swiper-button-prev'
+  },
+  breakpoints: {
+    567: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
   }
 });
