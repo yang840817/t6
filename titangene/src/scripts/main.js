1b67b754a013d26c4faf7c14f1502542e6ebf820
titangene <titangene.tw@gmail.com>
2020-04-21 14:22:03 +0800

    feat: change is-fixed-top jquery addStyle to addClass

--- titangene/src/scripts/main.js
+++ titangene/src/scripts/main.js
@@ -1,16 +1,10 @@
-let $search = $('.search');
-let $navbar = $('.navbar');
 let $navPrimary = $('.nav--primary');
 
 $(window).scroll(function() {
-  if ($('.nav--primary').offset().top <= $(window).scrollTop()) {
-    $search.css('margin-left', 0).css('box-shadow', 'none');
-    $navbar.css('box-shadow', 'none');
-    $navPrimary.css('box-shadow', '1px 0 4px 0 rgba(0, 0, 0, 0.3)');
+  if ($navPrimary.offset().top <= $(window).scrollTop()) {
+    $navPrimary.addClass('is-fixed-top');
   } else {
-    $search.removeAttr('style');
-    $navbar.removeAttr('style');
-    $navPrimary.removeAttr('style');
+    $navPrimary.removeClass('is-fixed-top');
   }
 });
 
