3d57631df005b0510637d2ce04572ba46beb4c78
wendy.liu <aaa3520732@gmail.com>
2022-01-05 17:12:14 +0800

    修改swiper.js 連結使用的連結現在有問題

--- wendy_t6/js/swiper.js
+++ wendy_t6/js/swiper.js
@@ -1,31 +1,63 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    320: {
+      direction: "horizontal",
+      slidesPerView: 1,
     },
-    breakpoints: {
-        320: {
-            direction: 'horizontal',
-            slidesPerView: 1,
-        },
-        992: {
-            slidesPerView: 4,
-            spaceBetween: 24,
-            slidesPerGroup: 4,
-        },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+      slidesPerGroup: 4,
     },
-})
+  },
+});
+// const swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 30,
+//   slidesPerGroup: 4,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper_button_next",
+//     prevEl: ".swiper_button_prev",
+//   },
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 10,
+//       slidesPerGroup: 2,
+//     },
+//     576: {
+//       slidesPerView: 3,
+//       spaceBetween: 20,
+//       slidesPerGroup: 3,
+//     },
+//     992: {
+//       slidesPerView: 4,
+//       spaceBetween: 20,
+//       slidesPerGroup: 1,
+//     },
+//   },
+// });
