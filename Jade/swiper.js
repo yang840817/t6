6745c38a0e27df55601fafb4c85b6e43010d6071
Jade Chen <y3m45566@gmail.com>
2022-05-11 17:29:32 +0800

    fix swiper empty space

--- Jade/swiper.js
+++ Jade/swiper.js
@@ -3,18 +3,10 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  // speed: 400,
-  // spaceBetween: 100,
-
-  // Navigation arrows
   navigation: {
     nextEl: '.next__button',
     prevEl: '.prev__button',
   },
-
-  // Default parameters
-  // slidesPerView: 4,
-  // spaceBetween: 0,
 
   breakpoints: {
 
@@ -23,19 +15,14 @@ const swiper = new Swiper('.swiper', {
       spaceBetween: 0
     },
 
-    576: {
-      slidesPerView: 1,
-      spaceBetween: 0
-    },
-
     768: {
       slidesPerView: 2,
-      spaceBetween: 0
+      spaceBetween: 10
     },
 
     992: {
       slidesPerView: 4,
-      spaceBetween: 5
+      spaceBetween: 10
     }
   }
 
